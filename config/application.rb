5ef883408879a0dd54ab6fe2acd50fd2a7af9fe0
Light <light@MacBook-Pro-Light.local>
2022-02-18 02:06:30 +0300

    question controller

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module Askme
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Moscow'
+
+    config.i18n.default_locale = :en
+    config.i18n.locale = :ru
+
+    config.i18n.fallbacks = [:en]
   end
 end
