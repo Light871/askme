60b9015d7e56c2d3f198b25ef39748529377ea53
Light <light@MacBook-Pro-Light.local>
2022-02-17 05:04:57 +0300

    users + sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,56 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :new, :create]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
-    @users = [
-      User.new(
-      id: 1,
-      name: 'Light',
-      username: 'light',
-      avatar_url: 'https://99px.ru/sstorage/1/2022/01/image_13101221852284073844.gif'
-      ),
-      User.new(
-      id: 2,
-      name: 'Dark',
-      username: 'dark'
-      )
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_path, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_path, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @user = User.new(
-      name: 'Light',
-      username: 'light',
-      avatar_url: 'https://99px.ru/sstorage/1/2022/01/image_13101221852284073844.gif'
-      )
+    @questions = @user.questions.order(created_at: :desc)
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('22.02.2022')),
-      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('22.02.2022'))
-    ]
+    @new_question = @user.questions.build
+  end
 
-    @new_question = Question.new
+  private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
+  def load_user
+    @user ||= User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
 end
